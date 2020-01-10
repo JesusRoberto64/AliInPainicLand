48ba61d38acad93c40e59ada5322f5a41d79395b
JesusRoberto64 <40547350+JesusRoberto64@users.noreply.github.com>
2020-01-09 20:15:05 -0600

    Add files via upload

---
+++ Test_Script.gd
@@ -0,0 +1,19 @@
+extends Node
+
+
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	
+	if Input.is_action_pressed("ui_up"):
+		myFuncTest()
+		
+		pass
+	
+	pass
+
+func myFuncTest():
+	print("In fuction line1")
+	
+	pass
